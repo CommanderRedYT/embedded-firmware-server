4520fe610073249b85160afa19df2e271c6bd78a
CommanderRedYT <ccommanderred@gmail.com>
2025-09-10 21:56:48 +0200

    Show help if no args provided

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 ARGS=$@
 
-# check that arguments are provided
+# if no args, default to "--help"
 if [ -z "$ARGS" ]; then
-  echo "No arguments provided. Usage: ./run.sh <arguments>"
-  exit 1
+  ARGS="--help"
 fi
 
 docker-compose -f docker-compose.yml exec firmware-server /bin/sh -c "cd /app && yarn start $ARGS"
