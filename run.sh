d05fbb807a0f657b28ef759b7bd99ba87e2add63
CommanderRedYT <ccommanderred@gmail.com>
2025-09-10 23:06:55 +0200

    Fix run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ if [ -z "$ARGS" ]; then
   ARGS="--help"
 fi
 
-docker-compose -f docker-compose.yml exec firmware-server /bin/sh -c "cd /app && tsx src/index.ts $ARGS"
+docker-compose -f docker-compose.yml exec firmware-server /bin/sh -c "cd /app && node_modules/.bin/tsx src/index.ts $ARGS"
